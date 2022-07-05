1bb0ad59163860659ffdbcaf8ec7171265584cb7
kendallli587 <kendallli587@gmail.com>
2022-07-05 09:27:56 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+// Addition Calculator
+
+// Event Listener
+document.getElementById("btn").addEventListener("click", calcTotal);
+
+// Event Function
+function calcTotal() {
+    // Input
+    let num1 = +document.getElementById("num1").value;
+    let num2 = +document.getElementById("num2").value;
+
+    // Process
+    let total = num1 + num2;
+
+    // Output
+    document.getElementById("total").innerHTML = total;
+}
